2089e3e7f60e1ab7d125bd2e077b892b611eff23
bcsato <bcsato@gmail.com>
2018-03-03 11:48:47 +0100

    commit

--- datum1.php
+++
@@ -1,7 +0,0 @@
-<?php
-$date = new DateTime("now", new DateTimeZone("Europe/Paris"));
-echo $date->format(DateTime::ISO8601) . PHP_EOL; // 2016-12-08T10:42:59+0100
-
-$datei = DateTimeImmutable::createFromFormat(DateTime::ISO8601, $date->format(DateTime::ISO8601));
-echo $datei->format(DateTime::ISO8601) . PHP_EOL; // 2016-12-08T10:42:59+0100
-?>
